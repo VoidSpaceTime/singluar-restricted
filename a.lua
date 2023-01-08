c996f5ebbeb077531068c275fabe68eb8c1d60f1
space_time_@outlook.com <Space_Time_@outlook.com>
2023-01-09 01:40:31 +0800

    1

---
+++ a.lua
@@ -0,0 +1,201 @@
+_assets_model("Construct/Construct_FinePine", "1", "unit", tree)
+_assets_model("Construct/Construct_FinePine_2", "2", "unit", tree)
+_assets_model("Construct/Construct_FinePine_3", "3", "unit", tree)
+_assets_model("Construct/Construct_Tree_pine_1", "4", "unit", tree)
+_assets_model("Construct/Construct_Tree_pine_2", "5", "unit", tree) -- 不能用
+_assets_model("Construct/Construct_FinePineSnow", "6", "unit", tree) --雪松树
+_assets_model("Construct/Construct_NewOak", "7", "unit", tree)
+_assets_model("Construct/Construct_NewOak_2", "8", "unit", tree)
+_assets_model("Construct/Construct_NewOak_3", "9", "unit", tree)
+_assets_model("Construct/Construct_Tree_Sakura_1.mdx", "24", "unit", tree) -- 樱花树
+
+_assets_model("Construct/Construct_grass", "10", "unit", grass) --
+_assets_model("Construct/Construct_grassAnimated_草_草原", "11", "unit", grass) --
+_assets_model("Construct/Construct_grassAnimated_草_丛林", "12", "unit", grass) --
+_assets_model("Construct/Construct_grassAnimated_草_灌木", "13", "unit", grass) --
+_assets_model("Construct/Construct_grassAnimated_草_热带", "14", "unit", grass) --
+_assets_model("Construct/Construct_GrassFlowers_1", "15", "unit", grass) -- 带小花
+_assets_model("Construct/Construct_GrassFlowers_2", "16", "unit", grass) -- 黄
+_assets_model("Construct/Construct_GrassFlowers_3", "17", "unit", grass) -- 类苔藓
+_assets_model("Construct/Construct_GrassFlowers_4", "18", "unit", grass) -- 黄大片
+_assets_model("Construct/Construct_GrassFlowers_5", "19", "unit", grass) -- 带小花大片
+_assets_model("Construct/Construct_shrubAnimated_草枯", "20", "unit", grass) --
+_assets_model("Construct/Construct_shrubAnimated_草绿", "21", "unit", grass) --
+_assets_model("Construct/Construct_Wheat", "22", "unit", grass) -- 黄
+_assets_model("Construct/Construct_WindBlownTree_小树", "23", "unit", grass) -- 小树
+
+--小楼功能性类型
+_assets_model("Construct/Construct_指挥中心", "25", "unit", factory)
+_assets_model("Construct/Construct_指挥中心2", "26", "unit", factory)
+_assets_model("Construct/Construct_Building_Garnison_驻军", "27", "unit", factory)
+_assets_model("Construct/Construct_Building_Helipad_停机坪", "28", "unit", factory) --带直升机停机坪
+_assets_model("Construct/Construct_Building_TechCenter_指挥中心", "29", "unit", factory) --带雷达
+_assets_model("Construct/Construct_Building_ComCenter_信息中心", "30", "unit", factory)
+
+--工厂类型
+_assets_model("Construct/Construct_Assembly_Factory_装配工厂", "32", "unit", factory)
+_assets_model("Construct/Construct_Aviary_玻璃房", "33", "unit", factory)
+_assets_model("Construct/Construct_Budynek3_factory", "34", "unit", factory) --方楼
+_assets_model("Construct/Construct_Building_Mines_矿井厂", "35", "unit", factory) --
+_assets_model("Construct/Construct_Building_Powergenerator_发电厂", "36", "unit", factory) --
+_assets_model("Construct/Construct_Building_Refinery_炼油厂", "37", "unit", factory) --
+_assets_model("Construct/Construct_Building_TankFactory_坦克工厂", "38", "unit", factory) --
+_assets_model("Construct/Construct_Building_WeaponFactory_武器工厂", "39", "unit", factory) --
+_assets_model("Construct/Construct_DrillRig_钻机", "40", "unit", factory) --
+_assets_model("Construct/Construct_Refinery_油井", "41", "unit", factory) --
+_assets_model("Construct/Construct_InstallationFoundryV2_铸造厂", "42", "unit", factory) --
+_assets_model("Construct/Construct_NuclearReactor_核电站", "43", "unit", factory) --
+_assets_model("Construct/Construct_Shipyard_船坞", "44", "unit", factory) --
+_assets_model("Construct/Construct_Chemical_reactors", "45", "unit", factory) --
+
+--仓库类型
+_assets_model("Construct/Construct_Building_Warehouse_仓库", "47", "unit", factory) --方楼
+_assets_model("Construct/Construct_bunker_hangar", "48", "unit", building) --半圆柱
+_assets_model("Construct/Construct_Garages", "49", "unit", building) --方
+
+
+--大厦/居民楼类型
+_assets_model("Construct/Construct_Building_1", "51", "unit", building) --中
+_assets_model("Construct/Construct_Building_2", "52", "unit", building) --高
+_assets_model("Construct/Construct_Building_3", "53", "unit", building) --矮
+_assets_model("Construct/Construct_Building_4", "54", "unit", building) --圆柱
+--
+_assets_model("Construct/Construct_house_high", "56", "unit", building) --长方高
+_assets_model("Construct/Construct_house", "57", "unit", building) --长方矮
+--
+_assets_model("Construct/Construct_Ratusz_belltower", "59", "unit", building) --带钟楼
+_assets_model("Construct/Construct_Ratusz2_building", "60", "unit", building) --带钟楼
+
+---塔类型
+_assets_model("Construct/Construct_Building_EnergeticTower_高能塔红", "62", "unit", tower) --圆柱
+--
+_assets_model("Construct/Construct_Building_RadarTower_雷达塔", "64", "unit", tower) --
+_assets_model("Construct/Construct_EnergyTransferPillar_能量转移柱红", "65", "unit", tower) --
+_assets_model("Construct/Construct_EnergyTransferPillar_能量转移柱蓝", "66", "unit", tower) --
+_assets_model("Construct/Construct_EnergyTransferPillar_能量转移柱绿", "67", "unit", tower) --
+_assets_model("Construct/Construct_High_Beacon_高级缓冲塔", "68", "unit", tower) --
+_assets_model("Construct/Construct_Scanner_Blue_扫描仪蓝", "69", "unit", tower) --未充能
+_assets_model("Construct/Construct_Scanner_Green_扫描仪绿", "70", "unit", tower) --
+_assets_model("Construct/Construct_Scanner_Offline_扫描仪无", "71", "unit", tower) --
+_assets_model("Construct/Construct_Scanner_Red_扫描仪红", "72", "unit", tower) --
+_assets_model("Construct/Construct_Scanner_Blue_Alt_扫描仪蓝立场", "73", "unit", tower) --立场展开
+_assets_model("Construct/Construct_Scanner_Green_Alt_扫描仪绿立场", "74", "unit", tower) --立场展开
+_assets_model("Construct/Construct_Scanner_Red_Alt_扫描仪红立场", "75", "unit", tower) --立场展开
+--
+_assets_model("Construct/Construct_ScoutTower_观察塔", "77", "unit", tower_allow) --盖帽
+_assets_model("Construct/Construct_Tower_观察塔混凝土", "78", "unit", tower) --混凝土碉堡类型
+_assets_model("Construct/Construct_Tower_Improved_观察塔混凝土2", "79", "unit", tower) --混凝土空军指挥塔
+_assets_model("Construct/Construct_Tower_Improved_Green_观察塔混凝土绿", "80", "unit", tower) --混凝土空军指挥塔绿
+
+--军队系列
+_assets_model("Construct/Construct_Camp", "82", "unit", structure4x4) --带旗子(灰色)
+_assets_model("Construct/Construct_od_bunker", "83", "unit", structure) --
+
+--墙/栅栏类型
+_assets_model("Construct/Construct_fence_wood", "85", "unit", options)
+_assets_model("Construct/Construct_fence_wood_2", "86", "unit", options)
+_assets_model("Construct/Construct_fence_wood_3", "87", "unit", options)
+--
+_assets_model("Construct/Construct_BetonWall", "89", "unit", options) --2
+--
+_assets_model("Construct/Construct_Doodad_Concrete_wall", "91", "unit", WallLeftUp) --右下
+_assets_model("Construct/Construct_Doodad_Concrete_wall2", "92", "unit", WallRightUp) --左下
+_assets_model("Construct/Construct_Doodad_Concrete_wall3", "93", "unit", WallColumn) --竖
+_assets_model("Construct/Construct_Doodad_Concrete_wall4", "94", "unit", WallRow) --横
+--
+_assets_model("Construct/Construct_Fence_Big_栅栏金属墙", "96", "unit", options) --向外倾斜
+_assets_model("Construct/Construct_Fortified_Wall_加固金属墙", "97", "unit", options) --长
+_assets_model("Construct/Construct_Fortified_Wall_Segment_加固金属墙段", "98", "unit", options) --短
+_assets_model("Construct/Construct_FortifiedWallGrille_加固金属栅栏门", "99", "unit", options) --短带门
+_assets_model("Construct/Construct_Fence_Segment_栅栏金属", "100", "unit", options) --
+--
+_assets_model("Construct/Construct_Fence_metal", "102", "unit", WallRow) --横!
+_assets_model("Construct/Construct_Fence_metal_2", "103", "unit", WallColumn) -- 当竖着的吧
+_assets_model("Construct/Construct_Fence_metal_3", "104", "unit", WallRow) --
+_assets_model("Construct/Construct_Fence_metal_4", "105", "unit", WallRow) --带门
+--
+_assets_model("Construct/Construct_Building_DangerZone_路障", "107", "unit", options) --梯形
+_assets_model("Construct/Construct_MetalRust24_roadblocks", "108", "unit", options) --黑黄
+_assets_model("Construct/Construct_sand_沙袋", "109", "unit", options) --沙袋梯形丑
+_assets_model("Construct/Construct_sandbags_沙袋堆", "110", "unit", options) --沙袋堆
+_assets_model("Construct/Construct_Sandbags", "111", "unit", options) --沙袋4个
+
+
+--平台类型
+_assets_model("Construct/Construct_bunker_wall_stairs", "113", "unit", options)
+_assets_model("Construct/Construct_bunker_wall", "114", "unit", options)
+_assets_model("Construct/Construct_bunker_wallcorner", "115", "unit", options)
+--
+_assets_model("Construct/Construct_output_stairs", "117", "unit", options)
+_assets_model("Construct/Construct_output_wall", "118", "unit", options)
+_assets_model("Construct/Construct_output", "119", "unit", options)
+--
+_assets_model("Construct/Construct_Helipadbasev_停机坪2", "121", "unit", options)
+_assets_model("Construct/Construct_Platform_Black_平台黑", "122", "unit", options)
+_assets_model("Construct/Construct_Turret_Platform_炮塔平台", "123", "unit", options)
+
+-- 装饰物
+_assets_model("Construct2/Construct_Art_1.mdx", "124", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_2.mdx", "125", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_3.mdx", "126", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_4.mdx", "127", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_5.mdx", "128", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_6.mdx", "129", "unit", structure4x4)
+_assets_model("Construct2/Construct_Art_Tower_1.mdx", "132", "unit", structure4x4)
+
+-- 大2
+_assets_model("Construct2/Construct_Building_10.mdx", "133", "unit", building)
+_assets_model("Construct2/Construct_Building_11.mdx", "134", "unit", building)
+_assets_model("Construct2/Construct_Building_12.mdx", "135", "unit", building)
+_assets_model("Construct2/Construct_Building_13.mdx", "136", "unit", building)
+_assets_model("Construct2/Construct_Building_14.mdx", "137", "unit", building)
+_assets_model("Construct2/Construct_Building_15.mdx", "138", "unit", building)
+_assets_model("Construct2/Construct_Building_16.mdx", "139", "unit", building)
+_assets_model("Construct2/Construct_Building_17.mdx", "140", "unit", building)
+_assets_model("Construct2/Construct_Building_18.mdx", "141", "unit", building)
+_assets_model("Construct2/Construct_Building_19.mdx", "142", "unit", building)
+_assets_model("Construct2/Construct_Building_20.mdx", "143", "unit", building)
+_assets_model("Construct2/Construct_Building_21.mdx", "144", "unit", building)
+_assets_model("Construct2/Construct_Building_22.mdx", "145", "unit", building)
+_assets_model("Construct2/Construct_Building_5.mdx", "146", "unit", building)
+_assets_model("Construct2/Construct_Building_6.mdx", "147", "unit", building)
+_assets_model("Construct2/Construct_Building_7.mdx", "148", "unit", building)
+_assets_model("Construct2/Construct_Building_8.mdx", "149", "unit", building)
+_assets_model("Construct2/Construct_Building_9.mdx", "150", "unit", building)
+_assets_model("Construct2/Construct_Building_Gymnastics_1.mdx", "151", "unit", building)
+_assets_model("Construct2/Construct_Building_memorial_1.mdx", "152", "unit", building)
+_assets_model("Construct2/Construct_Building_Theater_1.mdx", "153", "unit", building)
+_assets_model("Construct2/Construct_Building_WaterCube_1.mdx", "154", "unit", building)
+-- 军营2
+_assets_model("Construct2/Construct_Camp_2.mdx", "155", "unit", options)
+-- 集装箱2
+_assets_model("Construct2/Construct_Container_Barrel_1.mdx", "156", "unit", structure2x2)
+_assets_model("Construct2/Construct_Container_blue_1.mdx", "157", "unit", structure2x2)
+_assets_model("Construct2/Construct_Container_box_wood_1.mdx", "158", "unit", structure2x2)
+_assets_model("Construct2/Construct_Container_green_1.mdx", "159", "unit", structure2x2)
+_assets_model("Construct2/Construct_Container_orange_1.mdx", "160", "unit", structure2x2)
+_assets_model("Construct2/Construct_Container_rad_1.mdx", "161", "unit", structure2x2)
+-- 传送设备2
+_assets_model("Construct2/Construct_Equipment_Transmitter_1.mdx", "162", "unit", options)
+_assets_model("Construct2/Construct_Factory_build_1.mdx", "163", "unit", options)
+_assets_model("Construct2/Construct_Fence_Metal_5.mdx", "164", "unit", options)
+-- 居民房2 
+_assets_model("Construct2/Construct_House_2.mdx", "165", "unit", options)
+_assets_model("Construct2/Construct_House_4.mdx", "166", "unit", options)
+_assets_model("Construct2/Construct_House_5.mdx", "167", "unit", options)
+-- 杂项2
+_assets_model("Construct2/Construct_Other_WirePoles_1.mdx", "168", "unit", options)
+_assets_model("Construct2/Construct_Other_WirePoles_2.mdx", "169", "unit", options)
+_assets_model("Construct2/Construct_platform_1.mdx", "170", "unit", options)
+_assets_model("Construct2/Construct_Platform_apron_1.mdx", "171", "unit", options)
+-- 雷达2
+_assets_model("Construct2/Construct_Radar_1.mdx", "172", "unit", options)
+_assets_model("Construct2/Construct_Radar_2.mdx", "173", "unit", options)
+-- 塔2
+_assets_model("Construct2/Construct_Tower_build_1.mdx", "174", "unit", tower)
+_assets_model("Construct2/Construct_Tower_build_2.mdx", "175", "unit", tower)
+_assets_model("Construct2/Construct_Tower_Sentry_1.mdx", "176", "unit", tower)
+-- 墙2
+_assets_model("Construct2/Construct_Wall_fence_energy_1.mdx", "177", "unit", options)
+_assets_model("Construct2/Construct_Wall_Fence_metal_1.mdx", "178", "unit", options)
+_assets_model("Construct2/Construct_Wall_Stone_1.mdx", "179", "unit", options)
